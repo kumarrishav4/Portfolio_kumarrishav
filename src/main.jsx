e487c0bf0003cb08efa485677074bf07cb2a07b6
kumar rishav <ikumarrishav@gmail.com>
2024-04-17 00:39:16 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,36 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home,About,Projects,Contact } from './pages/index';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+const router = createBrowserRouter({
+    path:"/3js_portfolio_kumarrishav/",
+    element : <App/>,
+    children:[
+        {
+        path:"/3js_portfolio_kumarrishav/",
+        element: <Home/>
+        },
+        {
+        path:"/3js_portfolio_kumarrishav/Contact",
+        element: <Contact/>
+        },
+        {
+            path:"/3js_portfolio_kumarrishav/About",
+            element: <About/>
+        },
+        {
+            path:"/3js_portfolio_kumarrishav/Projects",
+            element: <Projects/>
+        },
+
+
+    ]
+
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+    // <App/>
+);
