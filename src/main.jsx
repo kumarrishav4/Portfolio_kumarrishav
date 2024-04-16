1eb4b9a45d0aff95c9f2644aa0f685d064f36d01
kumar rishav <ikumarrishav@gmail.com>
2024-04-17 00:43:41 +0530

    cannot deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Home,About,Projects,Contact } from './pages/index';
-
-const router = createBrowserRouter({
-    path:"/3js_portfolio_kumarrishav/",
-    element : <App/>,
-    children:[
-        {
-        path:"/3js_portfolio_kumarrishav/",
-        element: <Home/>
-        },
-        {
-        path:"/3js_portfolio_kumarrishav/Contact",
-        element: <Contact/>
-        },
-        {
-            path:"/3js_portfolio_kumarrishav/About",
-            element: <About/>
-        },
-        {
-            path:"/3js_portfolio_kumarrishav/Projects",
-            element: <Projects/>
-        },
 
 
-    ]
-
-})
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
-    // <App/>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
